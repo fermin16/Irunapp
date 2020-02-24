0175bb70a0ebc9c548311412b812cdae64f32bcd
Fermin <fermin1bachiller@gmail.com>
2020-02-24 16:10:33 +0100

    Subida de conexion a base de datos a rama mapa

---
+++ app/src/main/java/com/example/myapplication/App.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(User.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
